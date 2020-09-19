a71accc841ccf79983819528a50f178402708571
gulifan <gulifan@dxy.cn>
2020-09-19 23:02:01 +0800

    feat: 引入 reset.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.js';
+import { GlobalStyle } from './style'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
