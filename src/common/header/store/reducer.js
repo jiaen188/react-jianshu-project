20e81f402b78e43787aa4b553048ec460045bbad
gulifan <gulifan@dxy.cn>
2020-10-04 22:01:52 +0800

    feat: immutable管理store中的数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,17 @@
 import * as constants from './constants'
+import { fromJS } from 'immutable'
 
-const defalutState = {
+const defalutState = fromJS({
   focused: false
-}
+})
 
 export default (state = defalutState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    // imutable 对象的set方法，会结合之前imutable对象的值
+    // 和这只的值，返回一个全新的对象
+    return state.set('focused', true)
   } else if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false)
   }
   return state
 }
