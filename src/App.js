1d1c189e4e70a97dbe3f1922df3692a1d3290f97
gulifan <gulifan@dxy.cn>
2020-10-07 17:19:13 +0800

    feat: 引入 react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/home'
+import Detail from './pages/detail'
 import store from './store'
 
 import Header from './common/header'
@@ -7,7 +10,15 @@ import Header from './common/header'
 function App() {
   return (
     <Provider store={store}>
-      <Header></Header>
+      <div>
+        <Header></Header>
+        <BrowserRouter>
+        <div>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/detail" exact component={Detail}></Route>
+        </div>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
